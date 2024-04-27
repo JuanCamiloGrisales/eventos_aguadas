525275b02a67424e24bc6461878924e7ab4ff8bf
JuanCamiloGrisales <grisalesariasjuancamilo@gmail.com>
2024-04-27 08:45:47 -0500

    Default secret key

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = '$h@y&li^)1ul4x58csze2-lm@!$41j@ponfh&6fh#0u%psk$7l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
